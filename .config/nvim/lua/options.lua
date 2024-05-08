50549d1ce5064197eaf17e74bfd91be5bbdd3248
gvghbhj <gvghbhj@proton.me>
2024-05-08 17:12:07 +0100

    changes to neovim config

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -74,3 +74,7 @@ vim.opt.expandtab = true
 
 vim.opt.splitright = true
 vim.opt.splitbelow = true
+
+vim.opt.title = true
+vim.opt.titlelen = 0 -- do not shorten title
+vim.opt.titlestring = 'nvim %{expand("%:p")}'
