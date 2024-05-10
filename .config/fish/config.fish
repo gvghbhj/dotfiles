f65c674a0a53cfeedcf7e52c642a60bfa0745aed
gvghbhj <gvghbhj@proton.me>
2024-05-10 22:51:52 +0100

    added some more comments as well as some extra git abbreviations in to config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,31 +4,47 @@ if status is-interactive
     #
     __postexec_notify_on_long_running_commands # script stored in .config/fish/functions/ which notifies using notify-send when a long command is completed.
 
+
     # abbreviations: 
+    #
+    # GENERAL:
     abbr --add n nvim
-    abbr --add g gcc -W -Wextra -std=gnu11 -ggdb3 -o 
+    abbr --add fs fastfetch
+    abbr --add ra ranger
+    abbr --add za zathura --fork
+    abbr --add ki killall
+    abbr --add se sudoedit
+
+    # C PROGRAMMING:
+    abbr --add v valgrind --tool=memcheck --leak-check=full --track-origins=yes -s 
+    abbr --add g gcc -W -Wextra -std=gnu11 -ggdb3 -pedantic-errors -lreadline -o 
+
+    # Mullvad VPN:
     abbr --add ms mullvad status
     abbr --add mc mullvad connect
     abbr --add md mullvad disconnect
     abbr --add mr mullvad relay
-    abbr --add v valgrind --tool=memcheck --leak-check=full --track-origins=yes -s 
-    abbr --add se sudoedit
-    abbr --add fs fastfetch
-    abbr --add ki killall
+
+    # pacman:
     abbr --add pas sudo pacman -S
-    abbr --add ra ranger
-    abbr --add za zathura --fork
     abbr --add par sudo pacman -Rcs
     abbr --add pac sudo pacman 
     abbr --add pasu sudo pacman -Syu
+
+    # git:
     abbr --add push git push origin
     abbr --add pull git pull origin 
     abbr --add cm git commit -a -m
+    abbr --add st git status
     
+
+
     # zoxide
     zoxide init fish | source
     alias cd='z'
     
+
+
     # bindings
     bind \eF 'fg; commandline -f repaint'
     bind \eZ 'zi; commandline -f repaint'
