ed1559a456287986b0d58e2c6c9d0e396c08ecb2
gvghbhj <gvghbhj@proton.me>
2024-05-10 19:20:26 +0100

    changes to eza alias which now uses -a

--- .config/fish/functions/ls.fish
+++ .config/fish/functions/ls.fish
@@ -1,4 +1,4 @@
-function ls --wraps='eza -l --icons=always --color=always --smart-group --time-style relative' --wraps='eza -l --icons=always --color=always --total-size --time-style relative' --description 'alias ls=eza -l --icons=always --color=always --time-style relative'
-  eza -l --icons=always --color=always --smart-group --time-style relative $argv
+function ls --wraps='eza -l --icons=always --color=always --smart-group --time-style relative -a' --wraps='eza -l --icons=always --color=always --total-size --time-style relative -a' --description 'alias ls=eza -l --icons=always --color=always --time-style relative -a'
+  eza -l --icons=always --color=always --smart-group --time-style relative -a $argv
         
 end
