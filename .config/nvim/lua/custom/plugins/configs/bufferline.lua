82515a92d86f061a67c51c02fe0ab80f660be4e8
gvghbhj <gvghbhj@proton.me>
2024-05-14 13:27:56 +0100

    fixed buffer move bug in neovim bufferline

--- .config/nvim/lua/custom/plugins/configs/bufferline.lua
+++ .config/nvim/lua/custom/plugins/configs/bufferline.lua
@@ -2,8 +2,8 @@ return {
   'akinsho/bufferline.nvim',
   event = { 'BufReadPost', 'BufAdd', 'BufNewFile' },
   keys = {
-    { '<Tab>', '<cmd>BufferLineCyclePrev<cr>', desc = 'Prev Buffer' },
-    { '<S-tab>', '<cmd>BufferLineCycleNext<cr>', desc = 'Next Buffer' },
+    { '<S-tab>', '<cmd>BufferLineCyclePrev<cr>', desc = 'Prev Buffer' },
+    { '<Tab>', '<cmd>BufferLineCycleNext<cr>', desc = 'Next Buffer' },
     { '<leader>x', '<cmd>bdelete<CR>', desc = 'close buffer' },
     { '<A-.>', '<cmd>BufferLineMoveNext<CR>', desc = 'move buffer to the right' },
     { '<A-,>', '<cmd>BufferLineMovePrev<CR>', desc = 'move buffer to the left' },
