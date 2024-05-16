31037f2f5a385a75f5a59bbc1eaf5aa9f5481fde
gvghbhj <gvghbhj@proton.me>
2024-05-16 22:29:34 +0100

    removed birthdays program program from being tracked, while i rewrite it

--- .local/bin/better_birthdays/f_readline.h
+++
@@ -1,8 +0,0 @@
-#ifndef F_READLINE_H
-#define F_READLINE_H
-#include <stdio.h>
-#include <ctype.h>
-
-int f_readline(FILE *fptr, char location[], int n);
-
-#endif // !DEBUG
--- .local/bin/better_birthdays/my_malloc.h
+++
@@ -1,9 +0,0 @@
-#ifndef DUP_MALLOC_H
-#define DUP_MALLOC_H
-
-#include <stdlib.h>
-#include <stdio.h>
-
-void *my_malloc(size_t size);
-
-#endif 
