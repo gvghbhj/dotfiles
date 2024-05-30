c55f5bc3d1a9273469bae7e56f07a13f15dd435a
gvghbhj <gvghbhj@proton.me>
2024-05-30 11:50:46 +0100

    adjusted mini.pick winconfig

--- .config/nvim/lua/kickstart/plugins/mini.lua
+++ .config/nvim/lua/kickstart/plugins/mini.lua
@@ -106,8 +106,8 @@ return {
 
       local win_config = function()
         vim.opt.cmdheight = 0
-        height = math.floor(0.618 * vim.o.lines) - math.floor(vim.api.nvim_win_get_height(0) / 5.3)
-        width = math.floor(0.618 * vim.o.columns) - math.floor(vim.api.nvim_win_get_width(0) / 9)
+        height = math.floor(0.48 * vim.o.lines)
+        width = math.floor(0.48 * vim.o.columns)
         return {
           anchor = 'NW',
           border = 'rounded',
