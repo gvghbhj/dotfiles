4b9f831e258e6a827d71b235c475a523a12a182b
gvghbhj <gvghbhj@proton.me>
2024-08-17 23:38:03 +0100

    Added options in user.js, and changed ags config to aylurs instead of HyprPanel, also changed wallpaper and so changed the hyprlock and hyprpaper configs accordingly

--- .local/bin/kitty_tty_clock.fish
+++ .local/bin/kitty_tty_clock.fish
@@ -1,5 +1,5 @@
 #!/bin/fish
 
 while true 
-  kitty +kitten panel -o background_opacity=0.0 --edge=background tty-clock -S -t -c -s -C 7
+  kitty +kitten panel -o background_opacity=0.0 --edge=background tty-clock -S -t -c -s -C 6
 end
